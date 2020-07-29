600d6e081151d0659df066d57cf9b6c0c5c9efbc
Eugene <e2.shut@qa-academy.by>
2020-07-30 00:27:44 +0300

    add quit log

--- tests/test_TC-quit.py
+++ tests/test_TC-quit.py
@@ -9,4 +9,5 @@ logger = Logger(logger="TC-quit").getlog()
 class TestStopTests:
     def test_TearDown(self):
         time.sleep(2)
+        logger.info("Trying to stop browser")
         StopBrowser.stop()
