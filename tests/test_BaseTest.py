b116f6aab5f940e242548ef9a4cd733d8ce9bb17
Eugene <e2.shut@qa-academy.by>
2020-07-29 16:36:41 +0300

    Add logger

--- tests/test_BaseTest.py
+++ tests/test_BaseTest.py
@@ -4,19 +4,24 @@ from utils import LinkOperations, ButtonOperations, StopBrowser, Wait, ElementOp
 import time
 from pageObjects.pages import DownloadPage, MainPage
 from pageObjects.SystemAsserts import fileIsDownloaded
+from logger.logger import Logger
 
 LOCAL = jsonGetter.GetJson.get("LOCAL")
 SITE = jsonGetter.GetJson.get("SITE")
 actualBrowser = jsonGetter.GetJson.get("actualBrowser")
 
+logger = Logger(logger="BaseTest").getlog()
 
 
 class TestRunbrowser():
     def test_runbrowser(self):
         BaseElement.RunBrowser(actualBrowser)
         LinkOperations.OpenLink(SITE)
+        logger.info("Test")
 
     def test_lang(self):
         MainPage.MainPage().setLang()
+
+
 
 
